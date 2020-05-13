bb18c18bfd78d1bc2f5e6bd14a34221b4dc617e1
AlexeyChernikov <coderast99@gmail.com>
2020-05-13 22:52:00 +0300

    Create unusua_dish.py

---
+++ python_programming_for_the_absolute_beginner/unusua_dish.py
@@ -0,0 +1,8 @@
+# Enter 2 dishes. The program combines them and turns out a new unprecedented dish
+
+first_dish = input('Enter the first dish: ')
+second_dish = input('Enter the second dish: ')
+
+print('New dish: ' + first_dish + second_dish)
+
+input('\nPress Enter to exit...')
