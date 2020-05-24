c26114ed53451929fb663843ce2e1030e81e0242
AlexeyChernikov <coderast99@gmail.com>
2020-05-24 23:39:48 +0300

    Added rainbow_of_btns_v2.py

--- learning_tkinter/README.md
+++ learning_tkinter/README.md
@@ -4,3 +4,8 @@
 **Description:** Write a program consisting of seven buttons whose colors correspond to the colors of the rainbow. When a button is clicked, the color code must be inserted in the text field, and the color name must be inserted in the label. Color codes in hexadecimal encoding: #ff0000-red, #ff7d00-orange, #ffff00 – yellow, #00ff00 – green, #007dff – light blue, #0000ff – blue, #7d00ff – purple.
 
 **Code:** [rainbow_of_btns.py](./rainbow_of_btns.py)
+
+### 2. Rainbow of buttons (using the parameters of the pack() method):
+**Description:** Modify the code of the previous program using the parameters of the pack() method.
+
+**Code:** [rainbow_of_btns_v2.py](./rainbow_of_btns_v2.py)
---
+++ learning_tkinter/rainbow_of_btns_v2.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+
+btns_colors = {
+    "red": '#ff0000',
+    "orange": '#ff7d00',
+    "yellow": '#ffff00',
+    "green": '#00ff00',
+    "light blue": '#007dff',
+    "blue": '#0000ff',
+    "purple": '#7d00ff'
+    }
+
+        
+class NewButton:
+    def __init__(self, master, color, code_color):
+        self.color = color
+        self.code_color = code_color
+        self.btn = Button(master, bg=self.code_color, activebackground=self.code_color, command=self.get_color, width=2)
+        self.btn.pack(anchor=NW, side=LEFT, padx=1, pady=1)
+
+    def get_color(self):
+        lbl["text"] = f"Color: {self.color}"
+        tb.delete(0, END)
+        tb.insert(0, self.code_color)
+
+
+root = Tk()
+root.title("Rainbow")
+root.geometry('180x68')
+root.resizable(False, False)
+
+lbl = Label(root, text="Select color!", justify='center')
+lbl.pack(fill=X)
+
+tb = Entry(root, justify='center')
+tb.pack(fill=X)
+
+for k, v in btns_colors.items():
+    NewButton(root, k, v)
+
+root.mainloop()
